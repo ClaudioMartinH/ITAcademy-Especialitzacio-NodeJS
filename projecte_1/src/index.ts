963f943929ad1baeaf4a177e22ef8481d82f01f1
Claudio Martin <klaudito46@gmail.com>
2024-04-23 00:30:31 +0200

    modificat index.ts

--- projecte_1/src/index.ts
+++ projecte_1/src/index.ts
@@ -1,11 +1,25 @@
+import chalk from "chalk";
+import { createInterface } from "readline";
+
+
 type tasca = {tasca: string, completada: boolean};
 
 const tasques: tasca[] = []
 
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
 function afegirTasca(): void {
     const tasca: string = "";
     const completada: boolean = false;
-    tasques.push({tasca: tasca, completada: completada});
+    rl.question(chalk.bgGrey("Escriu la tasca: "), (tasca) => {
+        tasques.push({ tasca, completada: false });
+        console.log(chalk.green.bold("Tasca afegida correctament\n"));
+        console.clear();
+        mostrarTasques();
+      });
 }
 
 function eliminarTasca(): void {
@@ -14,6 +28,21 @@ function eliminarTasca(): void {
 }
 
 function mostrarTasques(): [string, boolean][] {
+    console.log(chalk.yellow.bold("\n-----Tasques pendents-----\n"));
+  if (tasques.length === 0) {
+    console.log(chalk.greenBright("No hi han tasques per mostrar\n"));
+  } else {
+    tasques.forEach((tasca, index) => {
+      let estat = tasca.completada ? "✅" : "❌";
+      if (tasca.completada) {
+        console.log(
+          chalk.greenBright(`${index + 1}. ${estat} - ${tasca.tasca}`)
+        );
+      } else {
+        console.log(chalk.redBright(`${index + 1}. ${estat} - ${tasca.tasca}`));
+      }
+    });
+  }
     return tasques.map((tasca) => [tasca.tasca?? "", tasca.completada]);
 }
 
