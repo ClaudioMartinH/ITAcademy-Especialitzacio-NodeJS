9713916b53903f44354e6e5276aee51cbae48bc1
Claudio Martin <klaudito46@gmail.com>
2024-04-24 00:13:32 +0200

    canvis en index.ts

--- projecte_1/src/index.ts
+++ projecte_1/src/index.ts
@@ -1,34 +1,46 @@
 import chalk from "chalk";
 import { createInterface } from "readline";
 
+type tasca = { tasca: string; completada: boolean };
 
-type tasca = {tasca: string, completada: boolean};
-
-const tasques: tasca[] = []
+const tasques: tasca[] = [];
 
 const rl = createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+  input: process.stdin,
+  output: process.stdout,
+});
 
 function afegirTasca(): void {
-    const tasca: string = "";
-    const completada: boolean = false;
-    rl.question(chalk.bgGrey("Escriu la tasca: "), (tasca) => {
-        tasques.push({ tasca, completada: false });
-        console.log(chalk.green.bold("Tasca afegida correctament\n"));
-        console.clear();
-        mostrarTasques();
-      });
+  const tasca: string = "";
+  const completada: boolean = false;
+  rl.question(chalk.bgGrey("Escriu la tasca: "), (tasca) => {
+    tasques.push({ tasca, completada: false });
+    console.log(chalk.green.bold("Tasca afegida correctament\n"));
+    console.clear();
+    mostrarTasques();
+  });
 }
 
 function eliminarTasca(): void {
-    const index: number = 0;
-    tasques.splice(index, 1);
+  const index: number = 0;
+  rl.question(
+    chalk.magenta("Introdueix el número de tasca a eliminar: "),
+    (numeroTasca) => {
+      const index = parseInt(numeroTasca) - 1;
+      if (index >= 0 && index < tasques.length) {
+        tasques.splice(index, 1);
+        console.clear();
+        mostrarTasques();
+      } else {
+        console.log(chalk.redBright("Numero de tasca invàlid"));
+        eliminarTasca();
+      }
+    }
+  );
 }
 
 function mostrarTasques(): [string, boolean][] {
-    console.log(chalk.yellow.bold("\n-----Tasques pendents-----\n"));
+  console.log(chalk.yellow.bold("\n-----Tasques pendents-----\n"));
   if (tasques.length === 0) {
     console.log(chalk.greenBright("No hi han tasques per mostrar\n"));
   } else {
@@ -43,12 +55,30 @@ function mostrarTasques(): [string, boolean][] {
       }
     });
   }
-    return tasques.map((tasca) => [tasca.tasca?? "", tasca.completada]);
+  return tasques.map((tasca) => [tasca.tasca ?? "", tasca.completada]);
 }
 
 function tascaCompletada(): boolean {
-    const index: number = 0;
-    return tasques[index].completada;
+  const index: number = 0;
+  rl.question(
+    chalk.bgGrey("Introdueix el número de la tasca completada: "),
+    (numeroTasca) => {
+      const index = parseInt(numeroTasca) - 1;
+      if (index >= 0 && index < tasques.length) {
+        tasques[index].completada = true;
+        console.log(chalk.green.bold("Tasca completada\n"));
+      } else {
+        console.log(chalk.red("Numero de tasca invàlid\n"));
+      }
+    }
+  );
+  return tasques[index].completada;
 }
 
-export default { afegirTasca, eliminarTasca, mostrarTasques, tascaCompletada, tasques };
+export default {
+  afegirTasca,
+  eliminarTasca,
+  mostrarTasques,
+  tascaCompletada,
+  tasques,
+};
